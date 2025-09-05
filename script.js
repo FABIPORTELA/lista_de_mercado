adcbe883b07e2425699abee94982e451b7a1705c
FABIPORTELA <168192189+FABIPORTELA@users.noreply.github.com>
2025-09-05 10:51:58 -0300

    up01

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("item-form");
+    const input = document.getElementById("item-input");
+    const list = document.getElementById("shopping-list");
 
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        const itemText = input.value.trim();
+
+        if(itemText) {
+            const li = document.createElement("li");
+            li.textContent = itemText;
+
+            const deleteBtn = document.createElement("button");
+            deleteBtn.textContent = "Excluir";
+            deleteBtn.addEventListener("click", () => {
+                list.removeChild(li);
+            });
+
+            li.appendChild(deleteBtn);
+            list.appendChild(li);
+            input.value = "";
+        }
+    });
+});
